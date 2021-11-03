eaca54c434add0340648432d568317afc7553199
Katherine Iannaccone <katherineiannaccone@yahoo.com>
2021-11-03 11:49:06 -0600

    commit tree f2610bd8f5bd8df37b8ff32da2248b057e04bc90

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,39 @@
-# your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name, :happiness, :hygiene
+
+    attr_accessor :bank_account  
+
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness= (new_happiness)
+        if new_happiness >= 0 && new_happiness <=10
+            @happiness = new_happiness
+        elsif new_happiness < 0 
+            @happiness = 0
+        else 
+            @happiness = 10
+        end
+    end
+  
+    def hygiene=(new_hygiene)
+        if new_hygiene >= 0 && new_hygiene <=10
+            @hygiene = new_hygiene
+        elsif new_hygiene < 0 
+            @hygiene = 0 
+        elsif new_hygiene > 10
+            @hygiene = 10
+        end
+    end
+
+  
+end
+
+katherine = Person.new("Katherine")
+puts katherine.name
